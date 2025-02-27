3266e1ea43406003f88c7512cc99b817b1b0b1fe
QuiqueDogo <enrique.lgv93@gmail.com>
2025-02-27 11:43:43 -0600

    1er proyecto

---
+++ CreacionNombres/PrimerProyecto.py
@@ -0,0 +1,19 @@
+print('Hola mundo "" ')
+print(100)
+print(100+50)
+print("100+50")
+print("hola" + ' ' + 'mundo')
+print('me llamo \'luis\' ')
+print('esta es una linea \nY esta esa otra linea \n')
+print('\testa es una tercera linea') #tabulacion
+print('this isn\'t a number')
+print('Esto \\ es una barra ')
+
+#print(input('dime tu nombre:' )) #de adentro hacia afuera
+#print(input('dime tu apellido:' ))
+#print('Tu nombre completo es: '+ input('dime tu nombre:' ) + ' ' + input('dime tu apellido:')) #de izq a der
+
+
+palabra1 = input('Dime Tu nombre: ')
+palabra2 = input('Dime Tu apellido: ')
+print('El nombre completo es: ' + palabra1 + ' ' + palabra2)
