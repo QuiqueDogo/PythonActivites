8366e1574cfe2afa30fc92dc082b57bd67b50de5
QuiqueDogo <enrique.lgv93@gmail.com>
2025-03-03 16:53:09 -0600

    operadores de comparacion

---
+++ AdivinadorNumero/index.py
@@ -0,0 +1,15 @@
+# veremos operadores  de comparacion, logicos, control de flujo en py, loops, rangos, enumeradores, zip, random etc
+#### OPERADORES DE COMPARACION
+# == igual a
+# != diferente a
+# > mayor que
+# < menor que
+# >= mayor o igual que
+# <= menor o igual que
+
+
+
+
+
+
+
