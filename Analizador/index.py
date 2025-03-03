d5e5b120f63b35e1e13746454ebc3c208db6e8b1
QuiqueDogo <enrique.lgv93@gmail.com>
2025-03-03 15:24:59 -0600

    Analizador de textos

--- Analizador/index.py
+++ Analizador/index.py
@@ -85,9 +85,141 @@
 # print(eliminado)
 
 # ORDENAMIENTO
-lista = ['g','a','e','b']
-lista.sort()
-lista.reverse()
-# no se puede almacetar de momento asi no listanueva = lista.sort(),  mientras no la asigne a otra si se puede
-lista_nueva = lista
-print(lista)
+# lista = ['g','a','e','b']
+# lista.sort()
+# lista.reverse()
+# # no se puede almacetar de momento asi no listanueva = lista.sort(),  mientras no la asigne a otra si se puede
+# lista_nueva = lista
+# print(lista)
+
+
+# ### DICCIONARIO (dict)( en formato seria como un objeto en js pero aqui no se ordenan )  c = {'nombre':'valor1'},
+# Valores se pueden repetir pero la claves no
+# # c['nombre'] >>>  'valor1'
+# diccionario = { 'c1': 'valor1', 'c2':'valor2'}
+# # print(type(diccionario))
+# print(diccionario)
+#
+# resultado = diccionario['c1']
+# print(resultado)
+#
+# cliente = {'nombre': 'Juan', 'apellido':'Fuentes', 'peso': 88, 'talla': 1.76}
+# consulta = cliente['talla']
+# print(consulta)
+#
+# dic = { 'c1':55,'c2':[10,20,30], 'c3': {'s1':100, 's2':200}}
+# # print(dic['c2'][0])
+# print(dic['c3']['s1'])
+#
+# dic2 = {'c1':['a','b','c'], 'c2':['d','e','f']}
+# resultado2 = (dic2['c2'][1]).upper()
+# print(resultado2)
+#
+# # anidar elementos a diccionarios y sobreescribir
+# dic3 = {1:'a',2:'b'}
+# print(dic3)
+# dic3[3] = 'c'
+# dic3['4'] = 'c'
+# print(dic3)
+# dic3[2] = 'B'
+# print(dic3)
+#
+# print(dic3.keys()) #key
+# print(dic3.values()) #valores
+# print(dic3.items()) #tuples
+
+
+##### TUPLES (son iguales que las listas pero estas son inmutables y van con corchetes) suelen ocupas menos espacio de memoria,
+# mi_tuple = (1,2,3,4)
+# # mi_tuple = 1,2,3,4
+# print(type(mi_tuple))
+# t= (5,5.6,'fa',(10,20))
+# print(t)
+# print(t[3][0])
+#
+# mi_tuple = list(mi_tuple)
+# print(mi_tuple)
+#
+# t = (1,2,3,1)
+# r,x,y,z = t # tiene que tener la misma cantidad de elementos  listas y dic pueden hacer esto
+#
+# print(x,y,z)
+# print(t.count(1))
+#
+
+
+# ##### SET son como los diccionarios al declarar pero los elementos no se repiten y los valores son unicos, no se muestras por indicen
+# sus elementos son inmutables , no se pueden inclur listas i diccionarios en ssets
+
+# mi_set = set([1,2,3,4,5])
+# mi_set = {1, 2, 3, 4, 5}
+# print(type(mi_set))
+# print(mi_set)
+#
+# otro_set = {1,2,3}
+# print(type(otro_set))
+# print(otro_set)
+# # print(otro_set[1]) #no
+# print(len(mi_set))
+# print(2 in mi_set)
+#
+# # union de sets
+# s1= {1,2,3,4,5,6,7}
+# s2={3,4,5}
+# s3= s1.union(s2)
+# print(s3)
+# s1.add(2)
+# # s1.remove(2)
+# # s1.discard(2)
+# s1.discard(26)
+# s1.pop() #elimina el primer
+# s1.clear() #elimina el primer
+# print(s1)
+
+
+#####   BOOLEANOS solo false y true como todos los demas lenguajes, capitalizado
+# var1 = True
+# var2 = False
+#
+# print(type(var1))
+# print(var1)
+#
+# # numero = bool()
+# numero = [1,2,3,4]
+# control = 5 in numero
+# print(type(control))
+# print(control)
+# val = None
+
+#Proyecto del dia
+# Crear un programa que primero le pida al usuario que
+# ingrese un texto. Puede ser un texto cualquiera: un artículo entero, un párrafo, una frase, un
+# poema, lo que quiera. Luego, el programa le va a pedir al usuario que también ingrese tres
+# letras a su elección y a partir de ese momento nuestro código va a procesar esa información
+# para hacer cinco tipos de análisis
+
+frase = input('Ingrese un texto cualquierda a su eleccion( Parrafo, frase ): ')
+# frase ="Lorem Ipsum is a simply dummy text of the printing and typesetting industry b c Python"
+letras = input('Ingresa 3 letras a tu eleccion juntas sin espacios: ')
+
+letras = list(letras)
+bool_to_text = {True:"Si", False: "No"}
+# print(frase)
+# print(letras)
+print(f'Hay un numero de la primera letra en la frase: {frase.count(letras[0])}')
+print(f'Hay un numero de la primera letra en la frase: {frase.count(letras[1])}')
+print(f'Hay un numero de la primera letra en la frase: {frase.count(letras[2])}\n')
+total_palabras = frase.split(' ')
+# print(total_palabras)
+print(f'El total de palabras en tu frase es: {len(total_palabras)}')
+reversa = frase[::-1]
+reversaPalabras = frase.split(' ')
+reversaPalabras.reverse()
+newR = reversaPalabras
+# print(newR)
+# conjunto = ' '.join(reversaPalabras)
+print(f'La primera letra de la frase: {frase[0]} y la ultima es: {frase[-1]}')
+print(f'El texto al reves quedaria asi: {reversa}\n y por palabras quedaria: \n {' '.join(newR)}')
+
+print(f'La palabra Python se encuentra en el texto? {bool_to_text['Python' in frase]}')
+# print()
