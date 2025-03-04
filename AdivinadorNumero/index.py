cb683cb2ca7bf93cbc1d860d0bd5f8d8e42eec4a
QuiqueDogo <enrique.lgv93@gmail.com>
2025-03-04 16:56:27 -0600

    operadores de comparacion, hoy no pude avanzar mucho

--- AdivinadorNumero/index.py
+++ AdivinadorNumero/index.py
@@ -6,8 +6,30 @@
 # < menor que
 # >= mayor o igual que
 # <= menor o igual que
+#
+# mi_variable = 'hola mundo'
+# # mi_bool = 10==10
+# # mi_bool = 'Blanco'=='blanco'
+# # mi_bool = 'Blanco'.lower()=='blanco'
+# # mi_bool = 100.0 == 100
+# # mi_bool = 100.0 != 100
+# # mi_bool = 101 != 100
+# # mi_bool = 100>99
+# # mi_bool = 5 >= 5
+# mi_bool = 7 >= 6 & 14 > 2
+# print(mi_bool)
 
 
+#OPERADORES LOGICOS (and or not) & | !
+a=10
+b=5
+c=15
+
+# mi_bool = 4 < 5 < 6
+# mi_bool =( 4 < 5) and ( 5 < 6)
+# mi_bool =10 == 10  or 3 == 3
+mi_bool = 1 == 10  or 3 == 3
+print(mi_bool)
 
 
 
