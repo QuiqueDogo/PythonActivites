1ead316757bcd71f24ea3419f7194619a6c32850
QuiqueDogo <enrique.lgv93@gmail.com>
2025-03-05 16:58:19 -0600

    control de flujos

--- AdivinadorNumero/index.py
+++ AdivinadorNumero/index.py
@@ -12,26 +12,51 @@
 # # mi_bool = 'Blanco'=='blanco'
 # # mi_bool = 'Blanco'.lower()=='blanco'
 # # mi_bool = 100.0 == 100
-# # mi_bool = 100.0 != 100
-# # mi_bool = 101 != 100
+# # mi_bool = 100
+# .0 != # mi_bool = 101 != 100
 # # mi_bool = 100>99
 # # mi_bool = 5 >= 5
 # mi_bool = 7 >= 6 & 14 > 2
 # print(mi_bool)
 
+#
+# #OPERADORES LOGICOS (and or not) & | !
+# a=10
+# b=5
+# c=15
+#
+# # mi_bool = 4 < 5 < 6
+# # mi_bool =( 4 < 5) and ( 5 < 6)
+# # mi_bool =10 == 10  or 3 == 3
+# # mi_bool = 1 == 10  or 3 == 3
+# texto = 'esta frase es breve'
+# mi_bool = ('frase' in texto) and ('breve' in texto)
+# print(mi_bool)
+#
+#
+#
+#
+##### CONTROL DE FLUJO  if HAY QUE RESPETAR LAS JERARQUIAS DE TABULACIONES
+# if 10 < 9:
+#     print('Es Correcto')
+# else:
+#     print('No ta chido')
 
-#OPERADORES LOGICOS (and or not) & | !
-a=10
-b=5
-c=15
+# mascota = 'perro'
+# if mascota == 'gato':
+#     print('Tienes un gato')
+# elif mascota == 'perro':
+#     print('Tienes un perro')
+# else:
+#     print('No se que animal es')
 
-# mi_bool = 4 < 5 < 6
-# mi_bool =( 4 < 5) and ( 5 < 6)
-# mi_bool =10 == 10  or 3 == 3
-mi_bool = 1 == 10  or 3 == 3
-print(mi_bool)
-
-
-
-
-
+edad = 16
+calificacion = 9
+if edad < 18:
+    print('Eres menor de edad')
+    if calificacion >= 7:
+        print('Aprobado')
+    else:
+        print('No Aprobado')
+else:
+    print('Eres adulto')
