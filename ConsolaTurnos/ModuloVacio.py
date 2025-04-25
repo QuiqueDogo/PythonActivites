fddcaf15d9c7c7aa632c7f8f9b049f1ac075cc8c
QuiqueDogo <enrique.lgv93@gmail.com>
2025-04-25 15:45:21 -0600

    paquetes y modulos

---
+++ ConsolaTurnos/ModuloVacio.py
@@ -0,0 +1,3 @@
+#instalar paquetes pypi pip, python packages - Busqueda pip install
+#un modulo es un archivo.py, estos se pueden usar en otros archivos y llamar a sus funciones
+# los paquetes son conjuntos de modulos, estos empiezan con __init__.py
